101bbe2641255c0533245fa278d7e60d005dbbbb
Luoshang Pan <panluoshang@live.com>
2017-10-22 14:30:08 -0400

    Optimize docker build and run script for psuedo distributed mode

--- prepare_host.sh
+++ prepare_host.sh
@@ -40,9 +40,4 @@ cp ~/.ssh/id_rsa . && \
 
 
 # Build Docker Image && Create Contrainer from Image
-docker build -t="pls331/centos:hbase-1.2.6-dist" . && \
-docker run -it --net=host \
-  -p 60000:60000 -p 60010:60010 -p 60020:60020 -p 60030:60030 \
-  -p 2888:2888 -p 3888:3888 -p 2181:2181 \
-  -p 21222:21222 \
-  pls331/centos:hbase-1.2.6-dist /etc/bootstrap.sh -bash
+docker build -t="pls331/centos:hbase-1.2.6-psuedo_distributed"
