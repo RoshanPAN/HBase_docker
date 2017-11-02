5b738512746c658cdfd24a985c2e5ecb4a650ce3
Luoshang Pan <panluoshang@live.com>
2017-11-02 10:58:31 -0400

    1. Fix java version 2. No blankspace in hbase-site.xml

--- prepare_host.sh
+++ prepare_host.sh
@@ -4,6 +4,7 @@
 # Allow ports in firewall
 # HMaster, hbase.master.port
 ufw allow 60000
+ufw allow 16000
 ufw allow 16010
 ufw allow 16020
 ufw allow 16030
@@ -54,7 +55,7 @@ cp ~/.ssh/id_rsa . && \
 docker build -t="pls331/centos:hbase-1.2.6-dist" . && \
 docker run -it --net=host \
   -p 60000:60000 -p 60010:60010 -p 60020:60020 -p 60030:60030 \
-  -p 16010:16010 -p 16020:16020 -p 16030:16030 \
+  -p 16010:16000 -p 16010:16010 -p 16020:16020 -p 16030:16030 \
   -p 16012:16012 -p 16022:16022 -p 16032:16032 \
   -p 16014:16014 -p 16024:16024 -p 16034:16034 \
   -p 16200:16200 -p 16300:16300 \
